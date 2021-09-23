061e4f7e8cfe892bf3739d54a660c3948fd38d89
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2021-09-23 11:19:46 -0700

    Add new prime number solution

--- PrimeNumbers.playground/Contents.swift
+++ PrimeNumbers.playground/Contents.swift
@@ -22,6 +22,15 @@
  
  - Note: This only goes up to 21 because the factorial for the computation gets very large very quickly. I used 64 bit usigned integers for the data type, but even that is not enough to contain the factorials.
  
+## Because the prime number checking can only go up to 21 with the above method, I added another solution
+ 
+### Alternative Solution
+ - Take in a number to check
+ - Check that number for a base case (ie: negative numbers, 0, 1, 2)
+ - Take that number and modulus it against all the number which precede it besides 1
+ - If any of the modulus's are 0, then it's not prime, and we return false
+ - Otherwise, return true
+ 
  Please note I use the underscore "_" in functions to override the need to provide an external name
  */
 
@@ -58,14 +67,47 @@ func CheckPrime(_ n: Int) -> Bool
     }
 }
 
+func CheckPrimeNaive(_ n: Int) -> Bool
+{
+    if(n < 0)
+    {
+        print("Negatives can't be prime")
+        return false
+    }
+    //Establish base case
+    if(n < 4)
+    {
+        return true
+    }
+    for i : Int in 2...(n/2)
+    {
+        if((n % i) == 0)
+        {
+            return false
+        }
+    }
+    return true
+}
+
 
 //Using a constant here to save memory
 //This only goes up to 21 before it breaks. I think this is because the factorial numbers to too large after that point.
 let cap : Int = 21 //The maximum factorial to be calculated
 
+print("Math Intensive Solutions:\n")
 
 for i : Int in 1...cap //I chose to use a for loop becuase it was best to loop a predetermined number of times while providing an incrementing value
 {
     print("Is \(i) prime: \(CheckPrime(i))")
+}
+
+
+let highCap : Int = 200
+
+print("\nNaive solutions:\n")
+
+for i : Int in 1...highCap //I chose to use a for loop becuase it was best to loop a predetermined number of times while providing an incrementing value
+{
+    print("Is \(i) prime: \(CheckPrimeNaive(i))")
 }
 
